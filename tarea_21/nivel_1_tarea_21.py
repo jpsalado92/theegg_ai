26c5c5acdbcd28576ada5eae9ed18699290bd15e
JP Salado <jpsalado92@gmail.com>
2021-06-02 13:56:49 +0200

    opt: Mejorar implementación tarea 21

--- tarea_21/nivel_1_tarea_21.py
+++ tarea_21/nivel_1_tarea_21.py
@@ -1,36 +1,52 @@
+"""
 # Solución de tarea_21
 # Autor: Juan Pablo Salado
-# Realizado: 2021-05-08
+# Fecha creación: 2021-05-08
+# Fecha última revisión: 2021-06-02
 
+# Tarea: Desarrollar un programa que dado un número introducido entre 0,0001 y 0,9999 (no más de 4 cifras decimales),
+obtenga y muestre la correspondiente fracción irreducible. Por ejemplo, el número 0,25 se puede obtener a partir de
+25/100, o de 2/8, o de 1/4, entre otros. La fracción irreducible es 1/4, que está formada por un numerador y un
+denominador que son primos entre sí.
+"""
 from fractions import Fraction
 
 
-def correct_input_check(input):
+def validate_input(value, verbose=False) -> bool:
+    """Valida el input para el programa asociado"""
     # Comprobar si el número es convertible a float
     try:
-        fval = float(input)
+        float_value = float(value)
     except ValueError:
-        print("Mete algo parecido a un número por favor")
+        if verbose:
+            print("Mete algo parecido a un número por favor")
         return False
 
     # Comprobar si el número está en el rango indicado
-    if fval < 0.0001 or fval > 0.9999:
-        print("Tío, ¡¡que escribas un valor en el rango 0,0001-0,9999!!\nTu número: ")
+    if float_value < 0.0001 or float_value > 0.9999:
+        if verbose:
+            print("Tío, ¡¡que escribas un valor en el rango 0,0001-0,9999!!\nTu número: ")
         return False
 
     # Comprobar si el número tiene menos del número máximo de decimales
-    if len(input) > 6:
-        print("¡Intenta un número con un máximo de 4 decimales!")
+    if len(str(value)) > 6:
+        if verbose:
+            print("¡Intenta un número con un máximo de 4 decimales!")
         return False
 
     # Manejando un número válido
     return True
 
 
+def get_fraction(value):
+    """Devuelve el numerador y denominador del número decimal introducido"""
+    return str(Fraction(float(value)).limit_denominator())
+
+
 if __name__ == "__main__":
-    val = input("Escribe un valor entre 0,0001 y 0,9999 con un máximo de 4 decimales:\nTu numero: ")
+    while True:
+        input_value = input("Escribe un valor entre 0,0001 y 0,9999 con un máximo de 4 decimales:\nTu numero: ")
+        if validate_input(input_value, verbose=True):
+            break
 
-    while not correct_input_check(val):
-        val = input()
-
-    print("La fracción que buscas es: " + str(Fraction(float(val)).limit_denominator()))
+    print("La fracción que buscas es: " + get_fraction(input_value))
